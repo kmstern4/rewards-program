0af26073a5def9a57447ec2821044f22d242012d
Kelly Stern <44009447+kmstern4@users.noreply.github.com>
2022-09-28 18:39:22 -0600

    adding header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  test('renders', async () => {
+    const {getByText} = render(<App/>);
+
+    expect(getByText('Customer Rewards Program')).toBeInTheDocument;
+  })
+})
