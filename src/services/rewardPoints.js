f861ca25580172734b24e1741702e840893bb065
Kelly Stern <44009447+kmstern4@users.noreply.github.com>
2022-09-28 19:57:29 -0600

    adding commented out api call

--- src/services/rewardPoints.js
+++ src/services/rewardPoints.js
@@ -2,10 +2,22 @@ import { purchases } from "../mock-data/mockPurchases";
 
 export const getRewardPoints = (customerId) => {
     try {
+        // const requestInit = {
+        //     method: 'GET',
+        //     headers: 'headers'
+        // };
+
+        // return await fetch(`api.endpoint.goes/here/${customerId}`, requestInit)
+        //     .then((response) => {
+        //         return response.json();
+        //     })
+        //     .catch((error) => {
+        //         throw error;
+        //     })
+
         const mockResponse = purchases.find(object => object.id === customerId);
 
         return mockResponse;
-    
     } catch (error) {
         throw error;
     }
